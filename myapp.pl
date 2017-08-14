b79558febb377e1eaa65cf9bc6c7ab085e30aede
skyter10086 <skyter10086@aliyun.com>
2017-08-14 10:10:14 +0800

    fixed db effectivity

--- myapp.pl
+++ myapp.pl
@@ -6,13 +6,13 @@ use strict;
 use Mojo::JSON qw(decode_json encode_json);
 use Data::Dumper;
 use Text::CSV_XS;
-
+use Encode;
       my $dbh = DBI->connect("dbi:SQLite::memory:","","",{sqlite_unicode =>1, AutoCommit => 1, RaiseError => 1})
                      or die $DBI::errstr;
-      my $statement_a = qq{ATTACH DATABASE 'A01.db' as 'A01'};
-      my $statement_b = qq{ATTACH DATABASE 'A02.db' as 'A02'};
-      my $statement_c = qq{ATTACH DATABASE 'DW.db' as 'DW'};
-		my $statement_d = qq{ATTACH DATABASE 'A03.db' as 'A03'};
+      my $statement_a = qq{ATTACH DATABASE '../SQL_Backup/A01.db' as 'A01'};
+      my $statement_b = qq{ATTACH DATABASE '../SQL_Backup/A02.db' as 'A02'};
+      my $statement_c = qq{ATTACH DATABASE '../SQL_Backup/DW.db' as 'DW'};
+		my $statement_d = qq{ATTACH DATABASE '../SQL_Backup/A03.db' as 'A03'};
       my $rows = $dbh->do($statement_a)           or die $dbh->errstr;
       $rows =   $dbh->do($statement_b)           or die $dbh->errstr;
       $rows = $dbh->do($statement_c)           or die $dbh->errstr;
@@ -41,6 +41,7 @@ post 'upload' => sub{
     unless my $example = $c->param('file1');
   my $size = $example->size;
   my $name = $example->filename;
+  
   #my $save_filename = 'd:/sites_up/tmp/test_tmpfile';
   #open my $fh , "+>" , $save_filename;
   #$example->move_to( $save_filename);
@@ -50,6 +51,7 @@ post 'upload' => sub{
  my $sth = $dbh->prepare($stmt);
 #sub insert_with_csv{
   my $data = $example->slurp();
+  $data = Encode::encode("UTF-8",Encode::decode("gbk",$data)) unless my $flag = Encode::is_utf8($data);
   $data =~ s/'//mg;
   $data =~ s/"//mg;
   my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
@@ -62,7 +64,7 @@ while (my $row = $csv->getline ($fh1)) {
 	next if grep /dwbm/ ,@$row ;
 	$sth->execute(@{$row} ) or die $DBI::errstr ;
 	$i++;
-	$dbh->commit if $i%100==0 && $i>100;
+	$dbh->commit if $i%10000==0 && $i>10000;
    }
  #}
 =pod
